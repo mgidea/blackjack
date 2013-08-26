2cf67f1e172f767ae6a31ff76bff29c07a8029b3
Tom Corley <tomcorleycarpentry@gmail.com>
2013-08-26 11:39:42 -0400

    added new card class creator

---
+++ card_class.rb
@@ -0,0 +1,16 @@
+class Card
+  def initialize(rank = nil, suit = nil)
+    if suit.nil?
+      @suit = ['♠', '♣', '♥', '♦'].sample
+    else
+      @suit = suit
+    end
+    if rank.nil?
+      @rank = ["1","2","3","4","5","6","7","8","9","10"].sample
+    else
+    @rank = rank
+    end
+    puts "Create a new card: #{@rank} of #{@suit}"
+  end
+end
+ Card.new
